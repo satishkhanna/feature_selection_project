327a8a7c48a296d359005e5f227778ba8e62c4a2
satishkhanna <snykhanna@gmail.com>
2017-10-29 18:45:28 +0000

    Done

--- q04_select_from_model/build.py
+++ q04_select_from_model/build.py
@@ -6,5 +6,16 @@ import numpy as np
 
 data = pd.read_csv('data/house_prices_multivariate.csv')
 
+np.random.seed(9)
 
+def select_from_model (df):
+    X, y = df.iloc[:,:-1], df.iloc[:,-1]
+    model = RandomForestClassifier()
+    sfm = SelectFromModel(model)
+    sfm.fit(X,y)
+    featurelist = sfm.get_support(indices=True)
+    full_set = X.columns.values
+    temp = full_set[featurelist]
+    feature_name = temp.tolist()
+    return feature_name
 # Your solution code here
