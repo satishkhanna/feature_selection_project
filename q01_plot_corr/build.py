30e0931573d5cc8a2695c9bad7bf6e05822c9283
satishkhanna <snykhanna@gmail.com>
2017-10-29 18:07:05 +0000

    Done

--- q01_plot_corr/build.py
+++ q01_plot_corr/build.py
@@ -1,8 +1,13 @@
 # Default imports
 import pandas as pd
 from matplotlib.pyplot import yticks, xticks, subplots, set_cmap
+import seaborn as sns
+from matplotlib import pyplot as plt
 
 data = pd.read_csv('data/house_prices_multivariate.csv')
 
-
+def plot_corr (df,size = 11):
+    subplots(figsize=(size,5))
+    sns.heatmap(data.corr(),cmap = 'YlOrRd')
+    plt.show()
 # Write your solution here:
--- q02_best_k_features/build.py
+++ q02_best_k_features/build.py
@@ -1,12 +1,20 @@
 # Default imports
 
 import pandas as pd
+import numpy as np
 
 data = pd.read_csv('data/house_prices_multivariate.csv')
 
 from sklearn.feature_selection import SelectPercentile
 from sklearn.feature_selection import f_regression
 
-
 # Write your solution here:
-
+def percentile_k_features (df,k=20):
+    X, y = df.iloc[:,:-1], df.iloc[:,-1]
+    fs = SelectPercentile(f_regression, percentile=k)
+    fs.fit_transform(X, y)
+    support = fs.get_support()
+    full_set = X.columns.values
+    temp = full_set[support]
+    finallist = temp.tolist()
+    return finallist
